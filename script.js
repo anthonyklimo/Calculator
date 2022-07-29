a0297415123a710c894d506216f7b11a353091e3
Anthony <cooperkl317@gmail.com>
2022-07-28 23:19:50 -0700

    add functionality to operator buttons and partial top display functionality

--- script.js
+++ script.js
@@ -1,70 +1,85 @@
-let bottomNumber = "0";
-let topEquation = "0";
+let currentValue = "0";
+let previousValue = "0";
+let topExpression = "";
+let isOperatorActive = false;
 
 let displayBottom = document.getElementById("display-bottom");
 let displayTop = document.getElementById("display-top");
 
-displayBottom.innerHTML = bottomNumber;
-displayTop.innerHTML = topEquation;
+displayBottom.innerHTML = currentValue;
+displayTop.innerHTML = topExpression;
 
 const numberButtons = document.querySelectorAll("[data-num]");
-// operatorButtons: 
+const operatorButtons = document.querySelectorAll(".operator");
 const deleteButton = document.getElementById("delete");
 const toggleNegative = document.getElementById("toggle-negative");
 const decimal = document.getElementById("decimal");
 const clear = document.getElementById("clear");
+const equals = document.getElementById("equals");
+
+numberButtons.forEach(button => {
+    button.addEventListener("click", function() {
+        if(currentValue == "0") {
+            currentValue = "";
+        }
+        currentValue += button.dataset.num;
+        displayBottom.innerHTML = currentValue;
+    })
+})
+
+operatorButtons.forEach(button => {
+    button.addEventListener("click", function() {
+        topExpression += `${currentValue} ${button.textContent} `;
+        displayTop.innerHTML = topExpression;
+        if(isOperatorActive == false) {
+            isOperatorActive = true;
+        }
+    console.log(topExpression);
+    previousValue = currentValue;
+    currentValue = '0';
+    })
+})
 
 clear.addEventListener("click", function() {
-    bottomNumber = "0";
-    topEquation = "0";
-    displayBottom.innerHTML = bottomNumber;
-    displayTop.innerHTML = topEquation;
+    currentValue = "0";
+    topExpression = "";
+    displayBottom.innerHTML = currentValue;
+    displayTop.innerHTML = topExpression;
+    isOperatorActive = false;
 });
 
 decimal.addEventListener("click", function() {
-    if(!bottomNumber.includes(".")) {
-        bottomNumber += ".";
-        displayBottom.innerHTML = bottomNumber;
+    if(!currentValue.includes(".")) {
+        currentValue += ".";
+        displayBottom.innerHTML = currentValue;
     }
 })
 
 toggleNegative.addEventListener("click", function() {
-    if(bottomNumber.charAt(0) == "-") {
-        bottomNumber = bottomNumber.substring(1, bottomNumber.length);
+    if(currentValue.charAt(0) == "-") {
+        currentValue = currentValue.substring(1, currentValue.length);
     } else {
-        bottomNumber = "-" + bottomNumber;
+        currentValue = "-" + currentValue;
     }
-    displayBottom.innerHTML = bottomNumber;
+    displayBottom.innerHTML = currentValue;
 })
 
-numberButtons.forEach(element => {
-    element.addEventListener("click", function() {
-        if(bottomNumber == "0") {
-            bottomNumber = "";
-        }
-        bottomNumber += element.dataset.num;
-        displayBottom.innerHTML = bottomNumber;
-        console.log(bottomNumber);
-    })
+equals.addEventListener("click", function() {
+    let a = parseFloat(previousValue);
+    console.log(a);
+    let b = parseFloat(currentValue);
+    console.log(b);
+    currentValue = operate(a, b);
+    displayBottom.innerHTML = currentValue;
+
 })
-// for (let i = 0; i < 10; i++) {
-//     numberButtons[i].addEventListener("click", function() {
-//         if(bottomNumber == "0") {
-//             bottomNumber = "";
-//         }
-//         bottomNumber += numberButtons[i].dataset.num;
-//         displayBottom.innerHTML = bottomNumber;
-//         console.log(bottomNumber);
-//     })
-// };
 
 deleteButton.addEventListener("click", function() {   
-    bottomNumber = bottomNumber.substring(0, bottomNumber.length - 1);
-    if(bottomNumber =="") {
-        bottomNumber = "0";
+    currentValue = currentValue.substring(0, currentValue.length - 1);
+    if(currentValue =="") {
+        currentValue = "0";
     }
-    displayBottom.innerHTML = bottomNumber;
-    console.log(bottomNumber);
+    displayBottom.innerHTML = currentValue;
 });
 
 //operator functions
@@ -81,7 +96,15 @@ function divide(a, b) {
     return a / b;
 }
 
-function operator(a, b) {
-
+function operate(a, b) {
+    if(topExpression.includes("+")) {
+        return add(a, b);
+    } else if (topExpression.includes("-")) {
+        return subtract(a, b);
+    } else if (topExpression.includes("x")) {
+        return multiply(a, b);
+    } else {
+        return divide(a, b);
+    }
 }
 
